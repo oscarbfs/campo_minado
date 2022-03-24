56b03d2cb88edb87f2c0bb5415486be25e696ac4
oscarbfs <oscar.borgesfs@gmail.com>
2022-03-24 15:09:31 -0300

    Componente Tabuleiro

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,4 +1,6 @@
+import 'package:campo_minado/components/tabuleiro_widget.dart';
 import 'package:campo_minado/models/explosao_exception.dart';
+import 'package:campo_minado/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
 import '../components/resultado_widget.dart';
 import 'package:campo_minado/components/campo_widget.dart';
@@ -22,21 +24,6 @@ class CampoMinadoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Campo vizinho1 = Campo(linha: 1, coluna: 0);
-    vizinho1.minar();   
-
-    Campo vizinho2 = Campo(linha: 1, coluna: 1);
-    vizinho2.minar();   
-
-    Campo campo = Campo(linha: 0, coluna: 0);
-    campo.adicionarVizinho(vizinho1);
-    campo.adicionarVizinho(vizinho2);
-
-    try {
-      // campo.minar();
-      campo.alternarMarcacao ();
-    } on ExplosaoException {}
-
     return MaterialApp(
       home: Scaffold(
         appBar: ResultadoWidget(
@@ -44,8 +31,8 @@ class CampoMinadoApp extends StatelessWidget {
           onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: CampoWidget(
-            campo: campo,
+          child: TabuleiroWidget(
+            tabuleiro: Tabuleiro(linhas: 25, colunas: 15, qtdeBombas: 0),
             onAbrir: _abrir,
             onAlternarMarcacao: _alternarMarcacao,
           ),
